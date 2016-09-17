2af55a6ae89a8b54d9d70d1903f04b049273f29d
Wigy <wigy_opensource_developer@yahoo.com>
2016-09-17 14:57:18 +0200

    No more panics outside the bootstrap code and the event loop

--- iop_home_d/src/main.rs
+++ iop_home_d/src/main.rs
@@ -11,8 +11,8 @@ use std::collections::HashMap;
 
 pub trait Reactive
 {
-    fn register(&self, poll: &Poll, token: Token);
-    fn act(&mut self, event: &Event);
+    fn register(&self, poll: &Poll, token: Token) -> Result<()>;
+    fn act(&mut self, event: &Event) -> Result<()>;
 }
 
 pub struct Reactor {
@@ -22,20 +22,24 @@ pub struct Reactor {
 }
 
 impl Reactor {
-    pub fn new() -> Reactor {
-        Reactor {
+    pub fn new() -> Result<Reactor> {
+        let result = Reactor {
             next_token: 0, 
             reactives_by_token: HashMap::new(),
-            poll: Poll::new().unwrap(),
-        }
+            poll: try!(Poll::new()),
+        };
+        Ok(result)
     }
-    pub fn register(&mut self, reactive: Box<Reactive>) {
+    pub fn register(&mut self, reactive: Box<Reactive>) -> Result<()> {
         let t = self.next_token;
         self.next_token = t + 1;
         let token = Token(t);
-        (*reactive).register(&self.poll, token);
+        try!((*reactive).register(&self.poll, token));
 
-        self.reactives_by_token.insert(token, reactive);
+        let old_reactive = self.reactives_by_token.insert(token, reactive);
+        debug_assert!(old_reactive.is_none());
+
+        Ok(())
     }
     pub fn run(&mut self) -> ! {
         let mut events = Events::with_capacity(1024);
@@ -43,7 +47,7 @@ impl Reactor {
             self.poll.poll(&mut events, None).unwrap();
             for event in events.iter() {
                 if let Some(reactive) = self.reactives_by_token.get_mut(&event.token()) {
-                    (*reactive).act(&event);
+                    (*reactive).act(&event).unwrap();
                 }
             }
         }
@@ -53,41 +57,45 @@ impl Reactor {
 
 #[derive(Debug)]
 pub struct Port {
-    token: Token,
     server: TcpListener,
 }
 
 impl Port {
     pub fn new(address: &str) -> Result<Port> {
-        let token = Token(0);
         let addr = try!(address.parse());
         info!("Binding port {}", addr);
         let server = try!(TcpListener::bind(&addr));
 
-        let result: Result<Port> = Ok(Port { token: token, server: server });
-        result
+        let result = Port { server: server };
+        Ok(result)
     }
 }
 
 impl Reactive for Port {
-    fn register(&self, poll: &Poll, token: Token) {
-        poll.register(&self.server, token, Ready::readable(), PollOpt::edge()).unwrap();
+    fn register(&self, poll: &Poll, token: Token) -> Result<()> {
+        try!(poll.register(&self.server, token, Ready::readable(), PollOpt::edge()));
+        Ok(())
     }
-    fn act(&mut self, _: &Event)
+    fn act(&mut self, _: &Event) -> Result<()>
     {
-        info!("Accepting connection on {}", self.server.local_addr().unwrap());
-        let (mut stream, peer_addr) = self.server.accept().unwrap();
+        info!("Accepting connection on {}", try!(self.server.local_addr()));
+        let (mut stream, peer_addr) = try!(self.server.accept());
         debug!("Connection from {:?}", peer_addr);
-        stream.write_fmt(format_args!("Hello, World!")).unwrap();
+        try!(write!(&mut stream, "Hello, World!"));
+        Ok(())
     }
 }
 
 pub fn main() {
     env_logger::init().unwrap();
 
-    let port = Port::new("0.0.0.0:3000").unwrap();
-    let mut reactor = Reactor::new();
-    reactor.register(Box::new(port));
+    let port1 = Port::new("0.0.0.0:3000").unwrap();
+    let port2 = Port::new("127.0.0.1:3001").unwrap();
+
+    let mut reactor = Reactor::new().unwrap();
+    reactor.register(Box::new(port1)).unwrap();
+    reactor.register(Box::new(port2)).unwrap();
+
     reactor.run();
 }
 
